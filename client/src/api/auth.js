8e516fc1412cac4beb9616ea49f7733d6734a34f
emancht <emancht@gmail.com>
2025-04-23 19:43:12 +0600

    11

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  'https://final-assignment-fawn-ten.vercel.app/api';
 
 export const registerUser = async (userData) => {
   try {
